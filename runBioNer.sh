5824ad7c5188a82ef1a757e51fbfc4db5e155f27
ManalBah <52249615+ManalBah@users.noreply.github.com>
2022-02-16 00:30:20 +0000

    Add files via upload
    A script file to overcome the memory problem of annotating a large number of files.

---
+++ runBioNer.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#  Script.sh
+#  
+
+SourceDir=/Users/mpqf2ma5/Downloads/BioNerDS_2020/test_docs
+CONFIG_FILE=/Users/mpqf2ma5/Downloads/BioNerDS_2020/properties.conf
+TARGET_KEY="out-normal"
+
+echo "The script starts now."
+
+
+for file in "$SourceDir"/*
+do
+echo "------------------"
+echo "$file" 
+filename=$(basename -- "$file")
+echo "$filename" 
+REPLACEMENT_VALUE="/Users/mpqf2ma5/Downloads/BioNerDS_2020/out/${filename}"
+echo "$REPLACEMENT_VALUE" 
+sed -i "s|\($TARGET_KEY *= *\).*|\1$REPLACEMENT_VALUE|" $CONFIG_FILE
+java -Xms8G -Xmx8G -jar bionerds-1.0-SNAPSHOT-jar-with-dependencies.jar --properties properties.conf --text $file
+done
