1d1e98b8f7f0a72cd0997fcf6adc8070255e2edb
ManalBah <52249615+ManalBah@users.noreply.github.com>
2022-02-16 01:00:08 +0000

    Update runBioNer.sh

--- runBioNer.sh
+++ runBioNer.sh
@@ -3,8 +3,8 @@
 #  Script.sh
 #  
 
-SourceDir=/Users/mpqf2ma5/Downloads/BioNerDS_2020/test_docs
-CONFIG_FILE=/Users/mpqf2ma5/Downloads/BioNerDS_2020/properties.conf
+SourceDir=../BioNerDS_2020/test_docs
+CONFIG_FILE=../BioNerDS_2020/properties.conf
 TARGET_KEY="out-normal"
 
 echo "The script starts now."
@@ -16,7 +16,7 @@ echo "------------------"
 echo "$file" 
 filename=$(basename -- "$file")
 echo "$filename" 
-REPLACEMENT_VALUE="/Users/mpqf2ma5/Downloads/BioNerDS_2020/out/${filename}"
+REPLACEMENT_VALUE="../BioNerDS_2020/out/${filename}"
 echo "$REPLACEMENT_VALUE" 
 sed -i "s|\($TARGET_KEY *= *\).*|\1$REPLACEMENT_VALUE|" $CONFIG_FILE
 java -Xms8G -Xmx8G -jar bionerds-1.0-SNAPSHOT-jar-with-dependencies.jar --properties properties.conf --text $file
